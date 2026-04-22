eba2aa68f179513fc74738edb2954a1cce0ae11e
Jauhelihamies <s2401274@edu.bc.fi>
2026-04-22 16:46:29 +0300

    PlayerController

--- Duels/Assets/SCRIPTS/PLAYER.cs
+++ Duels/Assets/SCRIPTS/PLAYER.cs
@@ -1,32 +1,61 @@
+
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 public class PLAYER : MonoBehaviour
 {
-    private float movementX;
-    private float movementY;
-    private Rigidbody rb;
-    public float speed = 0;
+    [Header("Movement Settings")]
+    public float moveSpeed = 5.0f;
+    public float mouseSensitivity = 0.1f;
+    public Transform cameraTransform;
 
+    private CharacterController controller;
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+    private float xRotation = 0f;
 
-
-    void Start()
+    void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+        // Lukitse hiiri pelin keskelle
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-
-    void OnMove(InputValue movementValue)
+    // N‰m‰ metodit kutsutaan automaattisesti, jos PlayerInput-komponentin 
+    // Behavior on asetettu tilaan "Send Messages"
+    public void OnMove(InputValue value)
     {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        moveInput = value.Get<Vector2>();
     }
 
-    private void FixedUpdate()
+    public void OnLook(InputValue value)
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
+        lookInput = value.Get<Vector2>();
     }
 
+    void Update()
+    {
+        HandleRotation();
+        HandleMovement();
+    }
+
+    private void HandleRotation()
+    {
+        float mouseX = lookInput.x * mouseSensitivity;
+        float mouseY = lookInput.y * mouseSensitivity;
+
+        // Pystysuuntainen k‰‰ntyminen (kamera)
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // Vaakasuuntainen k‰‰ntyminen (koko hahmo)
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void HandleMovement()
+    {
+        // Liikesuunta suhteessa hahmon rintamasuuntaan
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
 }

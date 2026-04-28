50f5227ff8fc7a5c5528b779123fca82120c3555
Jauhelihamies <s2401274@edu.bc.fi>
2026-04-28 12:45:13 +0300

    Lose screen

--- Duels/Assets/SCRIPTS/BackUp.cs
+++ Duels/Assets/SCRIPTS/BackUp.cs
@@ -1,17 +1,34 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackUp : MonoBehaviour
 {
     public string GetBackToWork;
+
+    private bool timeRunning = false;
+    private float timePassed = 0.0f;
+    public float TargetTime = 5.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        timeRunning = true;
+    }
+    private void FixedUpdate()
+    {
+        if (timeRunning == true)
+        {
+            if (timePassed < TargetTime)
+            {
+                timePassed += Time.deltaTime;
+            }
+            if (timePassed >= TargetTime)
+            {
+                SceneManager.LoadScene(GetBackToWork);
+                timeRunning = false;
+
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
